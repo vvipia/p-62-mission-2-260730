4c42e5b4d934d60fedc2e76647f006e25361a035
GIoriousPurpose <vvipia@naver.com>
2026-08-12 08:59:16 +0900

    git init

---
+++ src/main/java/com/back/jumptospringchapter3/JumpToSpringChapter3Application.java
@@ -0,0 +1,13 @@
+package com.back.jumptospringchapter3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumpToSpringChapter3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JumpToSpringChapter3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/jumptospringchapter3/JumpToSpringChapter3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.jumptospringchapter3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumpToSpringChapter3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
