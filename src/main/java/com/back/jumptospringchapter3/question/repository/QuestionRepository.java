89e78e518a3707cb88f6e44247bef061a1cccef4
GIoriousPurpose <vvipia@naver.com>
2026-08-17 10:34:31 +0900

    3-13장 검색 기능 추가하기

--- src/main/java/com/back/jumptospringchapter3/question/repository/QuestionRepository.java
+++ src/main/java/com/back/jumptospringchapter3/question/repository/QuestionRepository.java
@@ -3,9 +3,27 @@ package com.back.jumptospringchapter3.question.repository;
 import com.back.jumptospringchapter3.question.entity.Question;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     @Override
     Page<Question> findAll(Pageable pageable);
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
+
+
+//이렇게 직접 쿼리로 검객 기능을 구현할 수 도 있다.
+//    @Query("select "
+//            + "distinct q "
+//            + "from Question q "
+//            + "left outer join SiteUser u1 on q.author=u1 "
+//            + "left outer join Answer a on a.question=q "
+//            + "left outer join SiteUser u2 on a.author=u2 "
+//            + "where "
+//            + "   q.subject like %:kw% "
+//            + "   or q.content like %:kw% "
+//            + "   or u1.username like %:kw% "
+//            + "   or a.content like %:kw% "
+//            + "   or u2.username like %:kw% ")
+//    Page<Question> findAllByKeyword(@Param("kw") String kw, Pageable pageable);
 }
--- src/main/java/com/back/jumptospringchapter3/question/service/QuestionService.java
+++ src/main/java/com/back/jumptospringchapter3/question/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.back.jumptospringchapter3.question.service;
 
+import com.back.jumptospringchapter3.answer.entity.Answer;
 import com.back.jumptospringchapter3.global.DataNotFoundException;
 import com.back.jumptospringchapter3.question.entity.Question;
 import com.back.jumptospringchapter3.question.repository.QuestionRepository;
@@ -10,7 +11,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.springframework.data.jpa.domain.Specification;
 
+import java.io.Serial;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,11 +30,12 @@ import java.util.Optional;
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
-    public Page<Question> getList (int page) {
+    public Page<Question> getList (int page, String kw) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-        return this.questionRepository.findAll(pageable);
+        Specification<Question> spec = search(kw);
+        return this.questionRepository.findAll(spec, pageable);
     }
 
     public Question getQuestion(Integer id) {
@@ -62,4 +72,24 @@ public class QuestionService {
         question.getVoter().add(siteUser);
         this.questionRepository.save(question);
     }
+
+    private Specification<Question> search(String kw) {
+        return new Specification<>() {
+            @Serial
+            private static final long serialVersionUID = 1L;
+            @Override
+            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                query.distinct(true);  // 중복을 제거
+                Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+                Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+                Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+                return cb.or(cb.like(q.get("subject"), "%" + kw + "%"), // 제목
+                        cb.like(q.get("content"), "%" + kw + "%"),      // 내용
+                        cb.like(u1.get("username"), "%" + kw + "%"),    // 질문 작성자
+                        cb.like(a.get("content"), "%" + kw + "%"),      // 답변 내용
+                        cb.like(u2.get("username"), "%" + kw + "%"));   // 답변 작성자
+            }
+        };
+    }
 }
+
